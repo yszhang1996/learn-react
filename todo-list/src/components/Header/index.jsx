9672fdd20e795a68594b205642e89362d37fefe7
zhang569926 <yszhang1996@outlook.com>
2021-01-21 17:12:51 +0800

    Header的Input增加回车事件

--- todo-list/src/components/Header/index.jsx
+++ todo-list/src/components/Header/index.jsx
@@ -4,10 +4,13 @@ import { Button } from 'antd';
 import './index.css'
 
 export default class Header extends Component {
+    onPressEnter = (e) => {
+        console.log(e.target.value);
+    }
     render() {
         return (
             <div className="header-container">
-                <Input placeholder="请输入您的任务名称，按回车键确认" />
+                <Input placeholder="请输入您的任务名称，按回车键确认" onPressEnter={this.onPressEnter} />
                 <Button type="primary">添加</Button>
             </div>
         )
