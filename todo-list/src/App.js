d8f4dea0544fde259cd700e7450f13be36c36591
yszhang1996 <yszhang1996@outlook.com>
2021-01-22 01:11:33 +0800

    解决ant design中e.target.value赋值问题: 当使用onPressEnter监听事件时，拿到的DOM.target.value，如果直接赋值DOM.target.value = ""，可以赋值成功，如果函数中带有this.props中的函数，如this.props.addTodo(value)，则无法给DOM.target.value赋值，解决办法，绕过target.value，使用setState

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,25 +7,30 @@ import Footer from './components/Footer'
 export default class App extends Component {
   state = {
     todos: [{
-      id: '001',
+      id: 3,
       todo: "吃饭",
       done: true,
     }, {
-      id: '002',
+      id: 2,
       todo: "睡觉",
       done: false,
     }, {
-      id: '003',
+      id: 1,
       todo: "打豆豆",
       done: false,
     }]
   }
+  addTodo = (todo) => {
+    const { todos } = this.state
+    const newTodos = [{ id: todos[0].id + 1, todo, done: false }, ...todos]
+    this.setState({ todos: newTodos })
+  }
   render() {
-    const {todos} = this.state
+    const { todos } = this.state
     return (
       <div className="App">
         <div className="app-container" >
-          <Header />
+          <Header addTodo={this.addTodo} />
           <List todos={todos} />
           <Footer />
         </div>
--- todo-list/src/components/Header/index.jsx
+++ todo-list/src/components/Header/index.jsx
@@ -4,14 +4,23 @@ import { Button } from 'antd';
 import './index.css'
 
 export default class Header extends Component {
-    onPressEnter = (e) => {
-        console.log(e.target.value);
+    state = {
+        value: ""
+    }
+
+    onPress = () => {
+        // event.persist()
+        const { value } = this.state
+        if (value === '') return
+        this.props.addTodo(value)
+        this.setState({ value: "" })
     }
     render() {
+        const { value } = this.state
         return (
             <div className="header-container">
-                <Input placeholder="请输入您的任务名称，按回车键确认" onPressEnter={this.onPressEnter} />
-                <Button type="primary">添加</Button>
+                <Input placeholder="请输入您的任务名称，按回车键确认" value={value} onChange={e => { this.setState({ value: e.target.value }) }} onPressEnter={this.onPress} />
+                <Button type="primary" onClick={this.onPress}>添加</Button>
             </div>
         )
     }
