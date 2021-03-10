e41780c8e116aee47049631c72b5dc578b824fa6
yszhang1996 <yszhang1996@outlook.com>
2021-03-10 17:28:45 +0800

    使用react-redux初始化

--- redux_test/src/components/Count/index.jsx
+++ redux_test/src/components/Count/index.jsx
@@ -1,45 +1,26 @@
 import React, { Component } from 'react'
-import store from '../../redux/store' //引入store，用于获取redux中保存的状态
-import { createIncrementAction, createDecrementAction,createIncrementAsyncAction } from '../../redux/count_action' //引入actionCreator，专门用于创建action对象
 
 export default class Count extends Component {
 
     state = { name: 'Count' }
 
-    // componentDidMount(){
-    //     //检测redux中状态的变化，只要变化，就调用render()
-    //     store.subscribe(() => {
-    //         // this.forceUpdate()
-    //         this.setState({})
-    //     })
-    // }
-
     increment = () => {
-        const { value } = this.selectNumber
-        store.dispatch(createIncrementAction(value * 1))
+        // const { value } = this.selectNumber
     }
     decrement = () => {
-        const { value } = this.selectNumber
-        store.dispatch(createDecrementAction(value * 1))
+        // const { value } = this.selectNumber
     }
     incrementIfOdd = () => {
-        const { value } = this.selectNumber
-        const count = store.getState()
-        if (count % 2 !== 0) {
-            store.dispatch(createIncrementAction(value * 1))
-        }
+        // const { value } = this.selectNumber
     }
     incrementAsync = () => {
-        const { value } = this.selectNumber
-        setTimeout(() => {
-            store.dispatch(createIncrementAsyncAction(value * 1,500))
-        }, 500);
+        // const { value } = this.selectNumber
     }
 
     render() {
         return (
             <div>
-                <h1>当前求和为：{store.getState()}</h1>
+                <h1>当前求和为：?</h1>
                 <select ref={c => this.selectNumber = c}>
                     <option value="1">1</option>
                     <option value="2">2</option>
