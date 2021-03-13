392eb0c0943cc1d0f4436bbdfd1173d5cfb898cd
yszhang1996 <yszhang1996@outlook.com>
2021-03-13 17:13:50 +0800

    react-redux优化之简写mapDispatchToProps

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -3,28 +3,63 @@ import CountUI from "../../components/Count";
 // 引入connect用于连接UI组件与redux
 import { connect } from "react-redux";
 // 引入count_action
-import { createIncrementAction, createDecrementAction,createIncrementAsyncAction } from "../../redux/count_action";
+import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from "../../redux/count_action";
 
 //mapStateToProps函数返回的对象中的key就作为传递给UI组件props的key，value就作为传递给UI组件props的value--状态
-function mapStateToProps(state) {
-    return { count: state }
-}
+// function mapStateToProps(state) {
+//     return { count: state }
+// }
+// const mapStateToProps = state => ({ count: state })
 
 //mapDispatchToProps函数返回的对象中的key就作为传递给UI组件props的key,value就作为传递给UI组件props的value--操作状态
-function mapDispatchToProps(dispatch) {
-    return {
-        jia(number) {
-            // 通知redux执行加法
-            dispatch(createIncrementAction(number))
-        },
-        jian(number) {
-            dispatch(createDecrementAction(number))
-        },
-        jiaAsync(number,time) {
-            dispatch(createIncrementAsyncAction(number,time))
-        }
-    }
-}
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         jia(number) {
+//             // 通知redux执行加法
+//             dispatch(createIncrementAction(number))
+//         },
+//         jian(number) {
+//             dispatch(createDecrementAction(number))
+//         },
+//         jiaAsync(number, time) {
+//             dispatch(createIncrementAsyncAction(number, time))
+//         }
+//     }
+// }
+// const mapDispatchToProps = dispatch => ({
+//     jia(number) {
+//         // 通知redux执行加法
+//         dispatch(createIncrementAction(number))
+//     },
+//     jian(number) {
+//         dispatch(createDecrementAction(number))
+//     },
+//     jiaAsync(number, time) {
+//         dispatch(createIncrementAsyncAction(number, time))
+//     }
+// })
 
 // 使用connect()()创建并暴露一个Count的容器组件
-export default connect(mapStateToProps, mapDispatchToProps)(CountUI)
+export default connect(
+    state => ({ count: state }),
+    // mapDispatchToProps的一般写法
+    // dispatch => ({
+    //     jia(number) {
+    //         // 通知redux执行加法
+    //         dispatch(createIncrementAction(number))
+    //     },
+    //     jian(number) {
+    //         dispatch(createDecrementAction(number))
+    //     },
+    //     jiaAsync(number, time) {
+    //         dispatch(createIncrementAsyncAction(number, time))
+    //     }
+    // })
+
+    // mapDispatchToProps的简写
+    {
+        jia: createIncrementAction,
+        jian: createDecrementAction,
+        jiaAsync: createIncrementAsyncAction,
+    }
+)(CountUI)
