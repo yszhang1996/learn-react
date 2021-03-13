92cd9d12dd09f8641b46545f49b7689cd541abdb
yszhang1996 <yszhang1996@outlook.com>
2021-03-13 17:27:47 +0800

    react-redux优化之Provider组件的使用

--- redux_test/src/index.js
+++ redux_test/src/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import store from './redux/store'
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'))
 
 // 检测redux中的状态的改变，如redux的状态发生了改变，那么重新渲染App组件
-store.subscribe(() => {
-    ReactDOM.render(<App />, document.getElementById('root'))
-})
+// 当使用react-redux时，由于connect已经为我们做了监听，所以可以注释掉store.subscribe
+// store.subscribe(() => {
+//     ReactDOM.render(<App />, document.getElementById('root'))
+// })
