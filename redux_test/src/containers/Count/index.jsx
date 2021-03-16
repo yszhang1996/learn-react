0510335dc295912008c62d194d0dfba50a42c73e
yszhang1996 <yszhang1996@outlook.com>
2021-03-16 16:47:31 +0800

    react-redux优化之合并UI组件和容器组件

--- redux_test/src/containers/Count/index.jsx
+++ redux_test/src/containers/Count/index.jsx
@@ -1,5 +1,7 @@
-// 引入Count的UI组件
-import CountUI from "../../components/Count";
+// // 引入Count的UI组件
+// import CountUI from "../../components/Count";
+import React, { Component } from 'react'
+
 // 引入connect用于连接UI组件与redux
 import { connect } from "react-redux";
 // 引入count_action
@@ -39,6 +41,48 @@ import { createIncrementAction, createDecrementAction, createIncrementAsyncActio
 //     }
 // })
 
+//定义UI组件
+class Count extends Component {
+
+    state = { name: 'Count' }
+
+    increment = () => {
+        const { value } = this.selectNumber
+        this.props.jia(value * 1)
+    }
+    decrement = () => {
+        const { value } = this.selectNumber
+        this.props.jian(value * 1)
+    }
+    incrementIfOdd = () => {
+        const { value } = this.selectNumber
+        if (this.props.count % 2 !== 0) {
+            this.props.jia(value * 1)
+        }
+    }
+    incrementAsync = () => {
+        const { value } = this.selectNumber
+        this.props.jiaAsync(value * 1,1000)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>当前求和为：{this.props.count}</h1>
+                <select ref={c => this.selectNumber = c}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>&nbsp;
+                <button onClick={this.increment}>+</button>&nbsp;
+                <button onClick={this.decrement}>-</button>&nbsp;
+                <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+                <button onClick={this.incrementAsync}>异步加</button>
+            </div>
+        )
+    }
+}
+
 // 使用connect()()创建并暴露一个Count的容器组件
 export default connect(
     state => ({ count: state }),
@@ -62,4 +106,4 @@ export default connect(
         jian: createDecrementAction,
         jiaAsync: createIncrementAsyncAction,
     }
-)(CountUI)
+)(Count)
